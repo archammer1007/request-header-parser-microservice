2b921bbd9b30cb2137db7fb6197de33b6bd1362a
Ben Martini <benmartini7C0@gmail.com>
2024-06-18 10:48:31 +0000

    feat: get user's ipaddress and add it to output

--- index.js
+++ index.js
@@ -24,6 +24,11 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function(req, res){
+  var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
+  res.json({ ipaddress: ip })
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
